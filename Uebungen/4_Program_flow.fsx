c91ee832ef54d676d5e1a6cd5f8a9326fc2ea525
Kevin Schneider <kevin-schneider@mutagene.de>
2019-08-22 11:27:03 +0200

    Add Task 4

---
+++ Uebungen/4_Program_flow.fsx
@@ -0,0 +1,98 @@
+// Task 1.1
+// Deklariere eine Funktion die eine Liste als Parameter bekommt. Ist die Liste leer, soll "Die Liste ist leer", andernfalls "Die Liste ist voll"
+// in der Konsole ausgegeben werden
+
+
+
+// Task 1.2
+// Deklariere eine Funktion die drei Zahlen als Parameter bekommt. Die groeßte der drei Zahlen soll als Ergebnis ausgegeben werden. Benutze dafuer die 'if ... else' oder 'match' Funktion.
+
+
+
+// Task 1.3
+// Deklariere eine Funktion, die ein Jahr als Parameter bekommt. Wenn es sich um ein Schaltjahr handelt, soll das Ergebnis 'true' sein, andernfalls 'false'.
+// http://www.freizeitrevue.de/kultur-unterhaltung/schaltjahre-berechnen
+
+
+
+// Task 2.1
+// Erstelle einen Discriminate Union Type 'Monat' mit den 12 Monaten als Felder.
+// Deklariere eine Funktion die eine Liste von Monaten als Input bekommt und fuer jeden Monat in
+// der Liste eine Beschreibung in der Konsole ausgibt.
+
+
+
+// Task 2.2
+// Erstelle einen Discriminate Union Type mit den Feldern Int vom Typ int, Float vom typ float und String vom Typ string.
+// Deklariere eine Funktion, die drei Inputparameter bekommt. Ein string[], ein float[] und ein int[]. Die Funktion soll alle
+// Werte der drei Arrays zu dem vorher erstellten Discriminate Union Type konvertieren und in den selben Array uebertragen.
+
+
+
+// Task 2.3
+// Wende deine Funktion aus 2.2 auf drei Arrays an und binde das Ergebnis an einen Namen.
+// Deklariere eine Funktion die einen Array deines Discriminate Union Types in eine int list, float list und string list aufteilt.
+// Teste die Funktion an dem Namebinding in 2.3.
+
+
+
+// Task 3.1 
+// Deklariere eine Funktion, die alle ganzen Zahlen von 1 bis n in der Konsole ausgibt ('printfn'). Benutze dafuer eine 'for' oder 'while' Loop oder eine rekursive Funktion.
+
+
+
+// Task 3.2
+// Sorge dafuer, dass die Funktion aus 2.1 auch fuer negative Zahlen funktioniert.
+
+
+
+// Task 3.3
+// Deklariere eine eigene 'List.map' Funktion. Verwende dafuer eine 'for'-Loop.
+
+
+
+// Task 3.4
+// Deklariere eine eigene 'List.map' Funktion. Verwende dafuer eine rekursive Funktion.
+
+
+
+// Task 3.5
+// Deklariere eine eigene 'List.fold' Funktion. Verwende dafuer eine rekursive Funktion.
+
+
+
+// Task 4.1
+// Deklariere eine Funktion, die eine Liste mit beliebigen Zahlen und eine Zahl als Parameter bekommt. Die Funktion soll ueberpruefen, ob zwei der Zahlen (auch mit sich selbst) in der Eingabeliste
+// zu der angegebenen Zahl aufaddiert werden koennen.
+
+
+
+// Task 4.2
+// Ein Schachbrett ist ein 8x8 Feld. Die Reihen und Spalten haben daher die Indices 0 - 7. Erstelle eine Funktion, die angibt,
+// ob zwei Damen die auf dem Feld stehen, sich schlagen koennen. Die Parameter der Funktion sollen zwei Tupel (int*int) sein.
+
+
+
+// Task 4.3
+// Deklariere eine Funktion, die einen Planeten als String und eine Anzahl an Sekunden als Float als Parameter nimmt.
+// Die Funktion soll das Alter in Jahren fuer den angegebenen Planeten geben. Beispiel: Wenn jemand 1,000,000,000 Sekunden
+// alt ist, soll die Funktion fuer Erdenjahre ~31.69 herausgeben.
+// 
+// Umlaufzeiten:
+// 
+// Erde   : Umlaufzeit 365.25     Erdentage, oder 31557600 Sekunden
+// Merkur : Umlaufzeit 0.2408467  Erdenjahre
+// Venus  : Umlaufzeit 0.61519726 Erdenjahre
+// Mars   : Umlaufzeit 1.8808158  Erdenjahre
+// Jupiter: Umlaufzeit 11.862615  Erdenjahre
+// Saturn : Umlaufzeit 29.447498  Erdenjahre
+// Uranus : Umlaufzeit 84.016846  Erdenjahre
+// Neptun : Umlaufzeit 164.79132  Erdenjahre
+
+
+
+//Extra
+//Deklariere eine Funktion, die einen Integer als Parameter bekommt und ihn in das Excel-Benennungssystem fuer Reihen konvertiert.
+//Dabei sollen die Zahlen von 1 - 26 den Buchstaben A - Z zugeordnet werden. Bei einer 27 soll dann ein weiterer Buchstabe hinzukommen (AA) und so weiter.
+//Beispiele: 5 -> E; 38 -> AL; 1337 -> AYK
+
